f118883e4c2a91f186ea3b24e5b3b8eaa7142528
Tandin Wangchen <taandinwangchen27@icloud.com>
2025-04-01 08:19:48 -0700

    special deal changes

--- src/page/DailySpecial/index.js
+++ src/page/DailySpecial/index.js
@@ -8,10 +8,10 @@ import DailySpecialCard from '../../Components/DailySpecialCard/DailySpecialCard
 
 export default function DailySpecial() {
     const cardsData = [
+        { day:'Monday', text: 'Dosa Special', image: mondayBG, price:'7.99' },
         { day:'Tuesday', text: 'Unlimited Chana Bhature', image: tuesdayBG, price:'12.99' },
-        // { day:'Wednesday', text: 'Curry Special', image: wednesdayBG, price:'13.99' },
-        // { day:'Thursday', text: 'Dosa Special', image: thursdayBG, price:'6.99' },
-        { day:'Thursday', text: 'Biriyani Special', image: mondayBG, price:'13.99' },
+        { day:'Wednesday', text: 'Curry Special', image: wednesdayBG, price:'13.99' },
+        { day:'Thursday', text: 'Biriyani Special', image: thursdayBG, price:'13.99' },
     ];
     return (
         <>
