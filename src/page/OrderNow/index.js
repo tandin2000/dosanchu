b574bb6350d3fd688c3dede18055ba41839b4f7a
Tandin Wangchen <taandinwangchen27@icloud.com>
2025-05-19 19:56:26 -0700

    url fixes

--- src/page/OrderNow/index.js
+++ src/page/OrderNow/index.js
@@ -8,12 +8,12 @@ import touchbistro from '../../Assets/Images/touch-bistro.png';
 
 export default function OrderNow() {
     const cardsDataPickUp = [
-        { text: 'touch bistro', image: touchbistro, link: 'https://www.touchbistro.com', btnInfo: 'Pick Up'},
+        { text: 'touch bistro', image: touchbistro, link: 'https://order.tbdine.com/pickup/52957/menu', btnInfo: 'Pick Up'},
     ]
     const cardsDataDeliver = [
-        { text: 'UBER EATS', image: uberImg, link: 'https://www.ubereats.com/ca/store/dosa-n-chutney/Q7mPOHLdS4q5KqaaTqU1kQ?ps=1', btnInfo: 'Order Now' },
-        { text: 'DoorDash', image: doordash, link: 'https://www.doordash.com', btnInfo: 'Order Now' },
-        { text: 'Skipthedishes', image: skipthedishes, link: 'https://www.skipthedishes.com', btnInfo: 'Order Now' },
+        { text: 'UBER EATS', image: uberImg, link: 'https://www.ubereats.com/ca/store/dosa-n-chutney/LhFkZW_PVZm4xZwC-sGNcw?srsltid=AfmBOoqRKTvXnOCOSeZgZPxolql3i65cOHNCYNdI9z93PhECD-cIV93p', btnInfo: 'Order Now' },
+        { text: 'DoorDash', image: doordash, link: 'https://www.doordash.com/en-CA/store/dosa-n-chutney-vancouver-29951385/?srsltid=AfmBOop2w27YDuuF3ShDRQuXQmulKmv0Cl2CXi20vpVfWJFa4dXu4PV9', btnInfo: 'Order Now' },
+        { text: 'Skipthedishes', image: skipthedishes, link: 'https://www.skipthedishes.com/dosa-n-chutney-victoria-dr', btnInfo: 'Order Now' },
     ];
     return (
         <>
